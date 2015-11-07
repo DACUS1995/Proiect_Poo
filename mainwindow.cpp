b586b0e58fcc99978a45d3e31891f74efc25b804
Surdoiu Tudor Marian <studormarian@yahoo.co.uk>
2015-11-07 13:51:40 +0800

    Inceputul

---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QObject>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+   QObject::connect(ui->pushButton_1,SIGNAL(clicked()),this,SLOT(change_text()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::change_text()
+{
+  QString txt = "Ai apasat pe buton!!"
+  ui->textEdit->setText(txt);
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+
+class QAction;
+class QActionGroup;
+class QLabel;
+class QMenu;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+
+private slots:
+    void change_text();
+
+
+};
+
+#endif // MAINWINDOW_H
