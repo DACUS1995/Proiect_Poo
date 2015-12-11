d81a28657ece54f4692d47c8ea95e423f1b18dc5
Surdoiu Tudor Marian <studormarian@yahoo.co.uk>
2015-12-12 01:33:11 +0800

    plot - first implementation

---
+++ mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMenuBar>
+#include "qcustomplot.h"
+
+class QAction;
+class QActionGroup;
+class QLabel;
+class QMenu;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void setupPlot();
+
+private:
+    Ui::MainWindow *ui;
+
+
+private slots:
+    void change_text();
+    void makePlot();
+
+    void horzScrollBarChanged(int value);
+    void vertScrollBarChanged(int value);
+    void xAxisChanged(QCPRange range);
+    void yAxisChanged(QCPRange range);
+
+};
+
+#endif // MAINWINDOW_H
